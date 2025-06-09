0d1e0980690a46c4a3504f6c181c57edcf15c68d
Imaad Malik <imaad@wearenwn.com>
2025-06-09 14:59:37 +0100

    build error 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { StageWiseDevTools } from "@/components/stagewise-toolbar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -26,7 +25,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <StageWiseDevTools />
         </ThemeProvider>
       </body>
     </html>
